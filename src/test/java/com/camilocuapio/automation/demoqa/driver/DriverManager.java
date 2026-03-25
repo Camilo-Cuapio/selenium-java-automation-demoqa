a38dcde658ec38e03044de20926a99b4b64e72c3
Camilo-Cuapio <cuapio.camilo@gmail.com>
2026-03-24 23:56:18 -0600

    DriverManager class added

---
+++ src/test/java/com/camilocuapio/automation/demoqa/driver/DriverManager.java
@@ -0,0 +1,29 @@
+package com.camilocuapio.automation.demoqa.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DriverManager {
+
+    public static WebDriver createDriver(){
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+
+        options.setExperimentalOption("prefs", prefs);
+
+        return new ChromeDriver(options);
+    }
+}
